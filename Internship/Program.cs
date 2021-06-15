37e2c1c74b07a19b66e301fd72017694a3cef8a2
Tigran <tiko.vayq@gmail.com>
2021-06-15 09:20:37 +0400

    Time Struct

--- Internship/Program.cs
+++ Internship/Program.cs
@@ -10,7 +10,75 @@ namespace Internship
     {
         static void Main(string[] args)
         {
+            Time time = Time.Input();
+            Time swap = Time.Input();
 
+            time = time.Swap(swap);
+
+            time.Print();
+            Console.ReadLine();
+        }
+
+        public struct Time
+        {
+            public int Hour { get; set; }
+            public int Minutes { get; set; }
+
+            public Time(int hour, int minute)
+            {
+                this.Hour = hour;
+                this.Minutes = minute;
+            }
+
+            public Time(Time time)
+            {
+                this = time;
+            }
+
+            public Time Swap(Time time)
+            {
+                Time temp;
+                temp = this;
+                this = time;
+                return this;
+            }
+
+            static public Time Input()
+            {
+                int h, m;
+                do
+                {
+                    Console.Write("Input hours in 24hour format: ");
+                } while (!int.TryParse(Console.ReadLine(), out h) || h < 0 || h > 23);
+                do
+                {
+                    Console.Write("Input minutes: ");
+                } while (!int.TryParse(Console.ReadLine(), out m) || m < 0 || m > 59);
+
+                return new Time(h, m);
+            }
+
+            public void Print()
+            {
+                if(this.Hour < 10)
+                {
+                    Console.Write($"0{this.Hour}");
+                }
+                else
+                {
+                    Console.Write(this.Hour);
+                }
+                Console.Write(" : ");
+
+                if (this.Minutes < 10)
+                {
+                    Console.Write($"0{this.Minutes}");
+                }
+                else
+                {
+                    Console.Write(this.Minutes);
+                }
+            }
         }
 
     }
