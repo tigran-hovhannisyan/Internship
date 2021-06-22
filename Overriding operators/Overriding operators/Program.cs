c5dba840983c2f5032b9da34a13952f450fe6088
Tigran <tiko.vayq@gmail.com>
2021-06-22 19:20:50 +0400

    Initial commit

---
+++ Overriding operators/Overriding operators/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Overriding_operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
